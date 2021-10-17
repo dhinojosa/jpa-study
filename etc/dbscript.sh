401a94f0a3e244fd83048e180a9ba673cff33cac
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-10-17 11:25:04 -0600

    Upgraded database to use hsqldb

---
+++ etc/dbscript.sh
@@ -0,0 +1,2 @@
+java -cp ~/.m2/repository/org/hsqldb/hsqldb/2.6.0/hsqldb-2.6.0.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 mydb
+java -cp ~/.m2/repository/org/hsqldb/hsqldb/2.6.0/hsqldb-2.6.0.jar org.hsqldb.util.DatabaseManagerSwing
